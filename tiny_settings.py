18ae6138879cf6cc170c79e9f35dfd84a9543ecd
John McPherson <john@localhost.localdomain>
2008-08-01 23:09:23 +1200

    consistent indent (replace tabs with spaces)
    ignore urls with "tinybot" in them
    
    put some settings into a separate file for ease of configuration

---
+++ tiny_settings.py
@@ -0,0 +1,14 @@
+# "channelname": "key" (or None)
+channels = {
+    'tinybottest': None,
+}
+
+# a string used for logging in after connecting... eg "login username password"
+x_login = None
+
+# where to write the files, one per channel (with a trailing slash)
+atomdir = "/home/perry/public_html/tinybot/"
+#atomdir = "/tmp/"
+
+# url to this directory (with a trailing slash)
+atomdir_url = "http://coders.meta.net.nz/~perry/tinybot/"
