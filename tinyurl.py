6d4c241fb935070d2a13e5b607c66cd008203315
Perry Lorier <perry@breeze.dah.crc.net.nz>
2008-09-12 19:03:07 +1200

    Try and catch more errors (and fail)

--- tinyurl.py
+++ tinyurl.py
@@ -72,9 +72,9 @@ def tiny(user,channel,msg):
                 print "url:",`x`,"tinyurl:",`r`
                 tinycache[x] = r
                 realurl[r] = x
-            except HTTPError, e:
+            except urllib2.HTTPError, e:
                 tinycache[x] = "HTTP Error %s: %s" % (e.code,e.msg)
-            except URLError, e:
+            except urllib2.URLError, e:
                 tinycache[x] = str(e.reason)
             except IOError, e:
                 tinycache[x] = e.strerror
