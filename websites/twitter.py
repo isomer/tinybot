9149a138ff8856508367df3527072f2bf1dc6a64
Perry Lorier <github@isomer.meta.net.nz>
2009-05-20 21:14:00 +1200

    Minor fixes to the twitter parser
    Mostly by Aristotle Pagaltzis <pagaltzis@gmx.de>, minor changes by
    Perry Lorier.
    
    Features are Aristotle, Bugs are all mine.

--- websites/twitter.py
+++ websites/twitter.py
@@ -3,11 +3,18 @@ import re
 website="twitter.com"
 
 def get_summary(url, page):
-	match = re.match('.*?<span class="entry-content">(.*?)</span>', page, re.DOTALL)
 
-	if match:
-		m=match.groups(1)
-		tweet = m[0].strip()
-		return tweet
+	user = re.match('.*twitter.com/([^/]*)(/.*)?', url, re.DOTALL)
+	if user:
+		user = '@%s' % user.groups(1)[0]
 	else:
-		return "[twitter]"
+		user = '<fail whale>'
+
+	match = re.match('.*?<span class="entry-content">(.*?)</span>', page, re.DOTALL)
+	if match:
+		m = match.groups(1)
+		tweet = m[0].strip().replace('</a>', '')
+		tweet=re.sub(r'<a [^>]*>', '', tweet)
+		return '%s: %s' % (user, tweet)
+	else:
+		return '[twitter %s]' % user
