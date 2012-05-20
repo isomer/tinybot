16e7933df01e9b89df082fa58a095ace8e2292bd
Alastair Porter <alastair@porter.net.nz>
2012-05-20 18:16:08 -0400

    Use twitter api

--- websites/twitter.py
+++ websites/twitter.py
@@ -1,20 +1,27 @@
 import re
+import urllib2
+import json
 
 website="twitter.com"
 
 def get_summary(url, page):
 
-	user = re.match('.*twitter.com/(?:#!/)?([^/]*)(/.*)?', url, re.DOTALL)
-	if user:
-		user = '@%s' % user.groups(1)[0]
-	else:
-		user = '<fail whale>'
+    user = re.match('.*twitter.com/(?:#!/)?([^/]*)(/.*)?', url, re.DOTALL)
+    if user:
+        user = '@%s' % user.groups(1)[0]
+    else:
+        user = '<fail whale>'
 
-	match = re.match('.*?<span class="entry-content">(.*?)</span>', page, re.DOTALL)
-	if match:
-		m = match.groups(1)
-		tweet = m[0].strip().replace('</a>', '')
-		tweet=re.sub(r'<a [^>]*>', '', tweet)
-		return '%s: %s' % (user, tweet)
-	else:
-		return '[twitter %s]' % user
+    urlparts = url.split("/")
+    if urlparts[-2] == "status":
+        tweetid = urlparts[-1]
+        url = "https://api.twitter.com/1/statuses/show.json?id=%s" % tweetid
+        d = urllib2.urlopen(url).read()
+        data = json.loads(d)
+        return data["text"].encode("ascii", "ignore")
+    else:
+        u = user[1:]
+        url = "https://api.twitter.com/1/users/lookup.json?screen_name=%s" % u
+        d = urllib2.urlopen(url).read()
+        data = json.loads(d)
+        return data[0]["status"]["text"].encode("ascii", "ignore")
